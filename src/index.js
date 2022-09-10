b932bc5469cb366f853315cb98d6f525afa0ce3b
romanchenko31 <romanchenko.zhen@gmail.com>
2022-09-10 15:07:43 +0300

    add style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 
-
 import countryHbs from './template/country.hbs';
 import countriesHbs from './template/countrys.hbs';
 
@@ -9,29 +8,32 @@ import * as PNotifyMobile from '@pnotify/mobile';
 import '@pnotify/core/dist/BrightTheme.css';
 import fetchCountries from './js/fetchCountries.js';
 
-
 const debounce = require('lodash.debounce');
 
 const div = document.querySelector('div');
 const input = document.querySelector('input');
 let names = null;
 
-
 input.addEventListener('input', debounce((e) => {
     names = e.target.value;
-    fetchCountries(names)
-        .then(condition);  
+    if (names) {
+        fetchCountries(names)
+            .then(condition)
+    } 
+   
   }, 500));
 
-
 function condition(value) {
-    if (value.length >= 2 && value.length <= 10) {
-        fetchCountries(names).then(addCountriesMarcup);
-    } else if (value.length === 1) {
-        fetchCountries(names).then(addCountryMarcup);
-    } else if (value.length > 10) {
-        fetchCountries(names).then(addNotificationWindow);
+    if (value) {
+        if (value.length >= 2 && value.length <= 10) {
+            fetchCountries(names).then(addCountriesMarcup);
+          } else if (value.length === 1) {
+            fetchCountries(names).then(addCountryMarcup);
+          } else if (value.length > 10) {
+            fetchCountries(names).then(addNotificationWindow);
+          }
     }
+        
 }
 
 function addCountryMarcup(value) {
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,14 @@
 export default function fetchCountries(searchQuery) {
-    return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
-        .then(response => { return response.json() });
+    if (searchQuery) {
+        return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
+            .then(response => {
+                if (response.ok) {
+                    console.log(response);
+                    return response.json();
+                } else {
+                    Promise.reject(alert('hello'));
+                }
+            });
+    } 
+    
 }
-
-
